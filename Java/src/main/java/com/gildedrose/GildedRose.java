a4fe50488570433943489eb5421dcc81db966dba
Dan Grøndahl Glavind <dgg@praqma.net>
2020-01-08 22:02:30 +0100

    lifting sulfurus

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -51,19 +51,17 @@ class GildedRose {
                     item.quality = 0;
                 }
             } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
+                } else {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                }
 
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     item.sellIn = item.sellIn - 1;
-                }
 
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
                             item.quality = item.quality - 1;
                         }
                     }
