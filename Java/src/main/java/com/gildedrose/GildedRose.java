6dd5c76cbcc4aa3dc02fba3200ae69f1fa590ad5
Dan Grøndahl Glavind <dgg@praqma.net>
2020-01-08 22:05:57 +0100

    converting to switch

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,21 +14,22 @@ class GildedRose {
     }
 
     private void doUpdateQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-            }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
+        switch (item.name) {
+            case "Aged Brie":
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
+
                 }
-            }
-        } else {
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -50,23 +51,23 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
-            } else {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                break;
+            case "Sulfuras, Hand of Ragnaros":
 
-                } else {
+                break;
+            default:
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
                 }
-            }
+                break;
         }
     }
 
